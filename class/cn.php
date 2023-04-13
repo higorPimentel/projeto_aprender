2c656cc30622de64c31ba4774614b54642d750f5
HigorPimentel <49642934+higorPimentel@users.noreply.github.com>
2023-04-13 11:56:39 -0300

    Update cn.php

--- class/cn.php
+++ class/cn.php
@@ -7,14 +7,7 @@ $user = 'root';
 $password = '';
 $db_name = 'projeto_aprender'; 
 
-/*
-$host = 'localhost';
-$user = 'u259419259_higor';
-$password = 'higor961844';
-$db_name = 'u259419259_bd_jogos'; 
 
-
-*/
 
      $cn = mysqli_connect($host, $user,$password,$db_name);
 ?>
